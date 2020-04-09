2a6bfeec2e5ed67b16a212a2f299abfc3c76754a
Viktor E <v.engelowski@gmail.com>
2020-04-09 23:50:06 +0200

    initial

---
+++ policeTestScript/emergencyInvincible.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+
+[assembly: Rage.Attributes.Plugin("emergencyInvincible", Description = "Makes all vehicles that are labeled as 'Emergency' as invincible and repairs them if damaged.", Author = "MakerMacher")]
+
+namespace emergencyInvincible
+{
+    public static class emergencyInvincible
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                GameFiber.Yield();
+
+                Vehicle[] veh = World.GetAllVehicles();
+
+                foreach (var i in veh)
+                {
+                    if (i.Class.ToString() == "Emergency")
+                    {
+                        if (i.IsDeformationEnabled == true)
+                        {
+                            GameFiber.StartNew(delegate
+                            {
+                                i.Repair();
+                                i.IsDeformationEnabled = false;
+                                i.CanBeDamaged = false;
+                                i.IsInvincible = true;
+                            });
+                        }
+                        else
+                        {
+                            i.Repair();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
